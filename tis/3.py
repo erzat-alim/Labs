2448bb3710726eb58d2866c1501a284dc7942f59
Касимов Асад <kasimovasad2@gmail.com>
2025-09-25 11:35:52 +0300

    code for 3 tis, 2 lr sis

---
+++ tis/3.py
@@ -0,0 +1,23 @@
+alphabet = [
+    [11, 'A'], [12, 'B'], [13, 'C'], [14, 'D'], [15, 'E'],
+    [21, 'F'], [22, 'G'], [23, 'H'], [24, 'I'], [25, 'K'],
+    [31, 'L'], [32, 'M'], [33, 'N'], [34, 'O'], [35, 'P'],
+    [41, 'Q'], [42, 'R'], [43, 'S'], [44, 'T'], [45, 'U'],
+    [51, 'V'], [52, 'W'], [53, 'X'], [54, 'Y'], [55, 'Z']
+]
+
+data = input("Введите текст или шифр: ")
+result = ""
+if (data[0].isdigit()):
+    for i in range(0, len(data)-1, 2):
+        symbol = int(data[i]+data[i+1])
+        for i in alphabet:
+            if (i[0] == symbol):
+                result += i[1]
+else:
+    for letter in data:
+        for i in alphabet:
+            if (i[1] == letter):
+                result += str(i[0])
+
+print(result)
