18bf2a2a211e72736c78670b7d5cd9b4d832f5d6
Otabek Nazarov <otabek@gmail.com>
2025-10-08 19:01:58 +0300

    3 lr tis like 4 but 3

--- tis/4.py
+++ tis/4.py
@@ -1,10 +1,15 @@
 import random
+import psutil
+import os
 
-random_floats = [round(random.uniform(1, 100), 3) for i in range(4)]
+process = psutil.Process(os.getpid())
+mem_mb = process.memory_info().rss / 1024.0 / 1024.0
+print(f"Memory usage: {mem_mb:.2f} MB")
+
+random_floats = [round(random.uniform(1, 100), 3) for i in range(20)]
+print(random_floats)
 for i in range(len(random_floats)):
     for j in range(len(random_floats)):
         if random_floats[i] < random_floats[j]:
             random_floats[i], random_floats[j] = random_floats[j], random_floats[i]
-        print(random_floats, i, j)
-
 print(random_floats)
