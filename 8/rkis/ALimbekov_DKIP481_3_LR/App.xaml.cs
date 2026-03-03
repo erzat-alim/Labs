354f7733545c5df558d6ea18acb9c9805afdfbad
Ауд-Нагатинская-819 Преподаватель <edu-msk22-819t@synergy.ru>
2026-03-03 15:16:04 +0300

    3 lr rkis started

---
+++ 8/rkis/ALimbekov_DKIP481_3_LR/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ALimbekov_DKIP481_3_LR
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
